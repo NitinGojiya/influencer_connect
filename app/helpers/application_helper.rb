2486af490907f618e821c075eb56c41def76afbe
nitin <nitingojiya2000@gmail.com>
2025-08-28 13:24:53 +0530

    format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def human_count(number)
+    return "0" if number.blank?
+
+    number = number.to_i
+    if number >= 1_000_000
+      "#{(number / 1_000_000.0).round(1)}M"
+    elsif number >= 1_000
+      "#{(number / 1_000.0).round(1)}K"
+    else
+      number.to_s
+    end
+  end
 end
