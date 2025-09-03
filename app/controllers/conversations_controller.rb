82f93ff07f0c957dedb21112e2f44e3d952bdf11
nitin <nitingojiya2000@gmail.com>
2025-09-03 13:20:27 +0530

    chat in redirect issue fix

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,11 +1,20 @@
 class ConversationsController < ApplicationController
   layout :choose_layout
-  def index
-    @user = Current.session.user
-    @conversations = Conversation
-      .includes(:messages, :sender, :receiver)
-      .where("sender_id = :user_id OR receiver_id = :user_id", user_id: @user.id).order_by_last_message
+    def index
+  @user = Current.session.user
+  @conversations = Conversation
+    .includes(:messages, :sender, :receiver)
+    .where("sender_id = :user_id OR receiver_id = :user_id", user_id: @user.id)
+    .order_by_last_message
+
+  # If `open_conversation_id` param exists, preload that conversation
+  if params[:open_conversation_id]
+    @conversation = Conversation.find(params[:open_conversation_id])
+    @messages = @conversation.messages.includes(:user)
+    @message = Message.new
   end
+end
+
 
   def show
     @user = Current.session.user
@@ -20,20 +29,25 @@ class ConversationsController < ApplicationController
     @users = User.where.not(id: @user.id) # list all except yourself
   end
 
-  def create
-    @user = Current.session.user
-    @conversation = Conversation.find_by(sender_id: @user.id, receiver_id: params[:conversation][:receiver_id]) ||
-                    Conversation.find_by(sender_id: params[:conversation][:receiver_id], receiver_id: @user.id)
+ def create
+  @user = Current.session.user
+  @conversation = Conversation.find_by(sender_id: @user.id, receiver_id: params[:conversation][:receiver_id]) ||
+                  Conversation.find_by(sender_id: params[:conversation][:receiver_id], receiver_id: @user.id)
 
-    unless @conversation
-      @conversation = Conversation.create(
-        sender_id: @user.id,
-        receiver_id: params[:conversation][:receiver_id]
-      )
-    end
-
-    redirect_to @conversation
+  unless @conversation
+    @conversation = Conversation.create(
+      sender_id: @user.id,
+      receiver_id: params[:conversation][:receiver_id]
+    )
   end
+
+  # Always redirect to index with the conversation pre-selected
+  redirect_to conversations_path(open_conversation_id: @conversation.id)
+end
+
+
+
+
   private
    def choose_layout
     if current_user&.has_role?(:business_owner)
