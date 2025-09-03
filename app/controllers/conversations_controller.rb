8dc438c08498511e141a83ce3ecac83a07bc09b1
nitin <nitingojiya2000@gmail.com>
2025-09-03 15:39:50 +0530

    convertions restriction

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,55 +1,75 @@
 class ConversationsController < ApplicationController
   layout :choose_layout
-    def index
-  @user = Current.session.user
-  @conversations = Conversation
-    .includes(:messages, :sender, :receiver)
-    .where("sender_id = :user_id OR receiver_id = :user_id", user_id: @user.id)
-    .order_by_last_message
 
-  # If `open_conversation_id` param exists, preload that conversation
-  if params[:open_conversation_id]
-    @conversation = Conversation.find(params[:open_conversation_id])
-    @messages = @conversation.messages.includes(:user)
-    @message = Message.new
+  before_action :set_user
+  before_action :set_conversation, only: [:show]
+  before_action :authorize_conversation!, only: [:show]
+
+  def index
+    @conversations = Conversation
+      .includes(:messages, :sender, :receiver)
+      .where("sender_id = :user_id OR receiver_id = :user_id", user_id: @user.id)
+      .order_by_last_message
+
+    # If `open_conversation_id` param exists, preload that conversation
+    if params[:open_conversation_id]
+      @conversation = Conversation.find(params[:open_conversation_id])
+      if authorized_conversation?(@conversation)
+        @messages = @conversation.messages.includes(:user)
+        @message = Message.new
+      else
+        redirect_to conversations_path, alert: "You are not authorized to view that conversation."
+      end
+    end
   end
-end
-
 
   def show
-    @user = Current.session.user
-    @conversation = Conversation.find(params[:id])
     @messages = @conversation.messages.includes(:user)
     @message = Message.new
   end
 
   def new
-    @user = Current.session.user
     @conversation = Conversation.new
     @users = User.where.not(id: @user.id) # list all except yourself
   end
 
- def create
-  @user = Current.session.user
-  @conversation = Conversation.find_by(sender_id: @user.id, receiver_id: params[:conversation][:receiver_id]) ||
-                  Conversation.find_by(sender_id: params[:conversation][:receiver_id], receiver_id: @user.id)
+  def create
+    @conversation = Conversation.find_by(sender_id: @user.id, receiver_id: params[:conversation][:receiver_id]) ||
+                    Conversation.find_by(sender_id: params[:conversation][:receiver_id], receiver_id: @user.id)
 
-  unless @conversation
-    @conversation = Conversation.create(
-      sender_id: @user.id,
-      receiver_id: params[:conversation][:receiver_id]
-    )
+    unless @conversation
+      @conversation = Conversation.create(
+        sender_id: @user.id,
+        receiver_id: params[:conversation][:receiver_id]
+      )
+    end
+
+    # Always redirect to index with the conversation pre-selected
+    redirect_to conversations_path(open_conversation_id: @conversation.id)
   end
 
-  # Always redirect to index with the conversation pre-selected
-  redirect_to conversations_path(open_conversation_id: @conversation.id)
-end
-
-
-
-
   private
-   def choose_layout
+
+  def set_user
+    @user = Current.session.user
+  end
+
+  def set_conversation
+    @conversation = Conversation.find(params[:id])
+  end
+
+  # Ensure current user is either sender or receiver
+  def authorize_conversation!
+    unless authorized_conversation?(@conversation)
+      redirect_to conversations_path
+    end
+  end
+
+  def authorized_conversation?(conversation)
+    conversation.sender_id == @user.id || conversation.receiver_id == @user.id
+  end
+
+  def choose_layout
     if current_user&.has_role?(:business_owner)
       "application"
     elsif current_user&.has_role?(:influencer)
